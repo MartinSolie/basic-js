7826857628c976ed1ba136d2be07467e1d1c802a
Anton Martynov <antonymartynov@gmail.com>
2020-06-03 16:17:38 +0300

    Updated transform-array with constants

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,18 +1,7 @@
-/*
-  I am not sure that I understood usecases correctly:
-  discardDiscarded: {
-      input: [1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5],
-      output: [1, 2, 3, 4, 5]
-  },
-  discardDoubled: {
-      input: [1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5],
-      output: [1, 2, 3, 1337, 4, 5]
-  }
-  Somehow in first case we've discarded element according to the original array
-  (both control sequences influenced the same `1337`), but in the second case, we
-  have doubled 1337 and then discarded one of them. So we used the newly generated
-  array. So may be [1, 2, 4, 5] should be an expected behaviour in the first case?
- */
+const DOUBLE_NEXT = '--double-next';
+const DOUBLE_PREV = '--double-prev';
+const DISCARD_NEXT = '--discard-next';
+const DISCARD_PREV = '--discard-prev';
 
 module.exports = function transform(arr) {
   if (!Array.isArray(arr)) {
@@ -22,28 +11,24 @@ module.exports = function transform(arr) {
   const result = [];
   for (let i = 0; i < arr.length; i += 1) {
     switch (arr[i]) {
-      case '--double-next':
+      case DOUBLE_NEXT:
         if (i + 1 < arr.length) {
           result.push(arr[i + 1]);
         }
         break;
-      case '--double-prev':
+      case DOUBLE_PREV:
         if (result.length > 0) {
           result.push(result[result.length - 1]);
         }
         break;
-      case '--discard-next':
+      case DISCARD_NEXT:
         i += 1;
-        // to correctly proccess following case:
-        // --discard-next, number, --discard-prev|--double-prev
-        // If it finds that the next element after the ignored one is one of the "-prev" commands,
-        // it'll jump over this command.
         if (i + 1 < arr.length
-            && (arr[i + 1] === '--discard-prev' || arr[i + 1] === '--double-prev')) {
+            && (arr[i + 1] === DISCARD_PREV || arr[i + 1] === DOUBLE_PREV)) {
           i += 1;
         }
         break;
-      case '--discard-prev':
+      case DISCARD_PREV:
         result.pop();
         break;
       default:
